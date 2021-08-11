4e166635e85bd17ba7a54ecd11098d96fe8a4abb
katsuyawakabayashi <kw@kmail.me>
2021-08-10 21:06:39 -0700

    Add redirect route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { ThemeProvider } from "@material-ui/core";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import AuthProvider from "./contexts/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -17,6 +22,7 @@ const App = () => {
             <PrivateRoute exact path="/" component={Home} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
+            <Redirect to="/" component={Home} />
           </Switch>
         </ThemeProvider>
       </AuthProvider>
